87e38cf44919041bd35e288ea66b65bd5cdee8b0
Concetto Vecchio <concettovecchio@gmail.com>
2014-06-17 08:13:42 +0200

    refactoring .gitignore

--- templates_c/bf4b4eb34258f1b9bb7761939e9feb2f89876184.file.fattura.tpl.php
+++ templates_c/bf4b4eb34258f1b9bb7761939e9feb2f89876184.file.fattura.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.19-dev, created on 2014-06-16 16:49:48
+<?php /* Smarty version Smarty-3.1.19-dev, created on 2014-06-17 08:12:28
          compiled from "./templates/fattura.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:1072826009539eeb5ca4a3f4-57542278%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -13,7 +13,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '9e6b070c8cb75a2b091a59dcbc2131b5d5a97bf5' => 
     array (
       0 => './templates/layout.tpl',
-      1 => 1402926641,
+      1 => 1402985272,
       2 => 'file',
     ),
   ),
--- templates_c/c0360d049dff10f364dfc53ba2cc3958abf6ee6d.file.index.tpl.php
+++ templates_c/c0360d049dff10f364dfc53ba2cc3958abf6ee6d.file.index.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.19-dev, created on 2014-06-16 15:53:49
+<?php /* Smarty version Smarty-3.1.19-dev, created on 2014-06-17 08:11:26
          compiled from "./templates/index.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:595052866539ee7c9279d31-59830449%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -13,7 +13,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '9e6b070c8cb75a2b091a59dcbc2131b5d5a97bf5' => 
     array (
       0 => './templates/layout.tpl',
-      1 => 1402926641,
+      1 => 1402985272,
       2 => 'file',
     ),
   ),
